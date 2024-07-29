06b6ec526b9fcb8ef9f485d95e82e1b3cb87b969
Leonard <remon.w.dev@gmail.com>
2024-07-29 22:56:57 +0200

    spent 2 hours fighting the type syste to close the program with an optional error, I'd say I won

--- src/templetizer.rs
+++ src/templetizer.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::fs::File;
+
+macro_rules! Void {
+	() => {
+		Option::<Dummy>::None
+	};
+}
+
+// God forsaken code here
+struct Dummy {}
+
+impl std::fmt::Debug for Dummy {
+	fn fmt(&self, _f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		return Ok(());
+	}
+}
+
+
+fn abort<T, U: std::fmt::Debug>(s: &str, err: Option<U>) -> T {
+	match err {
+		| Some(e) => eprintln!("Aborting: {}\n\t{:#?}", s, e),
+		| None => eprintln!("Aborting: {}", s),
+	}
+	std::process::exit(1);
+}
+
 fn main() {
-	println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+	match args.len() {
+		| 1 => abort("Not enough arguments: the first argument must be the target template", Void!()),
+		| 2 => abort("Not enough arguments: the second argument must be the type to complete the template with", Void!()),
+		| _ => (),
+	}
+
+	let target_filename: &str = &args[1];
+	let target_type: &str = &args[2];
+
+	let target_file = match File::open(target_filename) {
+		| Ok(f) => f,
+		| Err(e) => abort("Failed to open the file", Some(e)),
+	};
 }
