212e1a9cea8a92eb1295208a2493c97410ad2a8e
Leonard <remon.w.dev@gmail.com>
2024-07-31 18:56:19 +0200

    looping for each line, jeez

--- src/templetizer.rs
+++ src/templetizer.rs
@@ -1,5 +1,13 @@
 use std::env;
-use std::fs::File;
+use std::fs;
+use std::io;
+use std::collections;
+use std::io::BufRead; // i should not need this
+
+// constants
+const INTERNAL_WILDCARD: char = '*';
+const TEMPLATE_KEY_WORD_START: &str = "template<";
+const TEMPLATE_KEY_WORD_END: &str = ">";
 
 // God forsaken code here
 struct Dummy {}
@@ -20,6 +28,10 @@ fn abort<T, U: std::fmt::Debug>(s: &str, err: Option<U>) -> T {
 	std::process::exit(1);
 }
 
+fn parse_templated_tyoes(s: &String) -> collections::HashMap<String, String> {
+	
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
@@ -32,8 +44,29 @@ fn main() {
 	let target_filename: &str = &args[1];
 	let target_type: &str = &args[2];
 
-	let target_file = match File::open(target_filename) {
+	let target_file = match fs::OpenOptions::new().append(true).read(true).open(target_filename) {
 		| Ok(f) => f,
 		| Err(e) => abort("Failed to open the file", Some(e)),
 	};
+
+	// cuz reading is too hard without a reader
+	let mut reader = io::BufReader::new(target_file);
+
+	let mut templated_types;
+
+	// reading line by line
+	loop {
+		let mut line: String = String::new();
+		let read_result = reader.read_line(&mut line);
+		match read_result {
+			| Ok(0) => break,
+			| Err(e) => abort(&format!("Could not read from the file{target_filename}"), Some(e)),
+			| _ => (),
+		}
+		print!("{}", line);
+
+		if line.contains(TEMPLATE_KEY_WORD_START) {
+			templated_types = parse_templated_tyoes(&line);
+		}
+	}
 }
